d0d8c83fc5394c7809d6105c0ae3b26103897c9e
Ilyasir <ilyas.gereykhanov@bk.ru>
2024-03-09 11:08:20 +0300

    Parser

---
+++ components_parser/urls.py
@@ -0,0 +1,2 @@
+urls = {}
+urls["CPU"] = "https://www.nix.ru/price/price_list.html?section=cpu_all#c_id=161&fn=161&g_id=7&new_goods=0&page=all&sort=%2Bp8175%2B1605%2B7287%2B766%2B2326&spoiler=1&store=msk-0_1721_1&thumbnail_view=2"
